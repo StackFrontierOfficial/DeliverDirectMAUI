c6d7ec42fde341e0238417f2ccbe0ff75550ba4b
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:48:10 +0530

    Initial Push

---
+++ DeliverDirectMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DeliverDirectMAUI
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ DeliverDirectMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DeliverDirectMAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
